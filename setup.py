2961ab90b04fb68171b6d28a03423ff85854c14d
Kyle Gorman <kylebgorman@gmail.com>
2015-02-28 10:37:37 -0800

    nlup requirements bump

--- setup.py
+++ setup.py
@@ -7,6 +7,6 @@ setup(name="WheresYrHeadAt",
       author="Kyle Gorman",
       author_email="gormanky@ohsu.edu",
       url="http://github.com/cslu-nlp/WheresYrHeadAt/",
-      install_requires=["nlup >= 0.3.0", "nltk >= 3.0.0"],
-      dependency_links=["http://github.com/cslu-nlp/nlup/archive/master.zip#egg=nlup-0.3.0"],
+      install_requires=["nlup >= 0.4.0", "nltk >= 3.0.0"],
+      dependency_links=["http://github.com/cslu-nlp/nlup/archive/master.zip#egg=nlup-0.4.0"],
       packages=["wheresyrheadat"])
